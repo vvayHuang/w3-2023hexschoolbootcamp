ba653290ac69cba134a7efcb1ac1d7bddafec49a
wayHuang <88394654+vvayHuang@users.noreply.github.com>
2023-08-08 00:15:47 +0800

    add:favicon fix:html

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 import './assets/scss/all.scss';
+import 'https://ajax.googleapis.com/ajax/libs/jquery/3.7.0/jquery.min.js';
+import 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js';
 
 $(document).ready(function () {
     $('.burger-menu').click(function (e) {
